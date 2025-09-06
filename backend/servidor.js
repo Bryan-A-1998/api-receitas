9019baf6208a1453440aa78022aa039d5a0819d9
Bryan Alex <bryanalex464@gmail.com>
2025-09-06 20:07:57 -0300

    Implementado rotas no servidor

--- backend/servidor.js
+++ backend/servidor.js
@@ -4,9 +4,25 @@ const app = express();
 
 app.use(express.json());
 
+//variaveis de rotas
+const usuarioRotas = require('./rotas/usuarioRotas');
+const ingredienteRotas = require('./rotas/ingredienteRotas');
+const receitaRotas = require('./rotas/receitaRotas');
+
+//rotas
+    //rota teste servidor
 app.get('/', (req, res) => {
     res.send('API de Receitas funcionando!');
 });
+
+    //rotas usuario
+app.use('/usuarios',usuarioRotas);
+    
+    //rotas ingrediente
+app.use('/ingredientes',usuarioRotas);
+
+    //rotas receita
+app.use('/receitas',usuarioRotas);
 
 const PORTA = 7000;
 app.listen(PORTA, () => console.log(`Servidor rodando na porta ${PORTA}`));
