c5cf239ab7a7acd47fbcf7db949980e94cd80b0d
Bryan Alex <bryanalex464@gmail.com>
2025-09-06 21:34:11 -0300

    Utilização de modelos

--- backend/controladores/usuarioControlador.js
+++ backend/controladores/usuarioControlador.js
@@ -15,9 +15,15 @@ async function cadastrar(req, res) {
       return res.status(400).json({ mensagem: "Email já cadastrado" });
     }
 
-    const usuarioId = await UsuarioRepositorio.cadastrar(nome, email, senha);
+    const usuarioCadastrado = await UsuarioRepositorio.cadastrar(nome, email, senha);
 
-    res.status(201).json({ mensagem: "Usuário cadastrado com sucesso", usuarioId });
+    res.status(201).json({ mensagem: "Usuário cadastrado com sucesso", 
+      usuario: {
+        id: usuarioCadastrado.id,
+        nome: usuarioCadastrado.nome,
+        email: usuarioCadastrado.email
+      }
+     });
   } catch (erro) {
     console.error("Erro no cadastro:", erro);
     res.status(500).json({ mensagem: "Erro ao cadastrar usuário" });
@@ -39,7 +45,13 @@ async function login(req, res) {
       return res.status(401).json({ mensagem: "Senha incorreta, não foi possivel efetuar login" });
     }
 
-    res.status(200).json({ mensagem: "Login realizado com sucesso", usuarioId: usuario.id });
+    res.status(200).json({ mensagem: "Login realizado com sucesso", 
+      usuario: {
+        id: usuario.id,
+        nome: usuario.nome,
+        email: usuario.email
+      }
+     });
   } catch (erro) {
     console.error("Erro no login:", erro);
     res.status(500).json({ mensagem: "Erro ao realizar login" });
@@ -52,13 +64,19 @@ async function editar(req, res) {
     const { id } = req.params;
     const { nome, senha } = req.body;
 
-    const atualizado = await UsuarioRepositorio.editar(id, nome, senha);
+    const usuarioAtualizado = await UsuarioRepositorio.editar(id, nome, senha);
 
-    if (!atualizado) {
+    if (!usuarioAtualizado) {
       return res.status(404).json({ mensagem: "Usuário não encontrado" });
     }
 
-    res.status(200).json({ mensagem: "Usuário atualizado com sucesso" });
+    res.status(200).json({ mensagem: "Usuário atualizado com sucesso",
+      usuario: {
+        id: usuarioAtualizado.id,
+        nome: usuarioAtualizado.nome,
+        email: usuarioAtualizado.email
+      }
+     });
   } catch (erro) {
     console.error("Erro ao editar usuário:", erro);
     res.status(500).json({ mensagem: "Erro ao editar usuário" });
