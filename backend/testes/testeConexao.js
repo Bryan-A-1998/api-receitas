145df1ebbc2b0630c81bba08f30e8125a61fa2a8
Bryan Alex <bryanalex464@gmail.com>
2025-09-07 02:34:43 -0300

    Implementação e testes realizados, passo 2 fazer testes com erros forçados

---
+++ backend/testes/testeConexao.js
@@ -0,0 +1,9 @@
+const pool = require('../configuracao/conexaoBD');
+
+async function testar() {
+  const resultado = await pool.query('SELECT NOW()');
+  console.log('Hora do servidor PostgreSQL:', resultado.rows[0]);
+  pool.end();
+}
+
+testar();
